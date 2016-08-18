332fc3859d7b40ce59de8efddaa70f76fec36baa
siddiqur-rahman <siddiqur.rahman077@gmail.com>
2016-08-18 16:00:31 +0600

    partial implementation display numbers.

--- AslesTool/src/ca/com/salestool/SalesData.java
+++ AslesTool/src/ca/com/salestool/SalesData.java
@@ -4,6 +4,8 @@ public class SalesData {
 	int data[]={0,4,42};
 	
 	public void display(){
-		System.out.println("TODO: implement data display");
+		for(int i=0;i<data.length;i++){
+			System.out.println("");
+		}
 	}
 }
